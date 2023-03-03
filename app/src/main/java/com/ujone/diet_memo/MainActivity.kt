cbcfadb0198f28ebae5f04bf2044d1712086359b
ujoneMAC@HOME <03toot28@gmail.com>
2023-03-03 18:40:30 +0900

    Add Dialog, DateSelect

--- app/src/main/java/com/ujone/diet_memo/MainActivity.kt
+++ app/src/main/java/com/ujone/diet_memo/MainActivity.kt
@@ -1,7 +1,16 @@
 package com.ujone.diet_memo
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.widget.Button
+import android.widget.DatePicker
+import android.widget.ImageView
+import androidx.appcompat.app.AlertDialog
+import java.util.Calendar
+import java.util.GregorianCalendar
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,6 +18,39 @@ class MainActivity : AppCompatActivity() {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val writeButton = findViewById<ImageView>(R.id.writeBtn)
+        writeButton.setOnClickListener {
+
+            // 다이얼로그 처리
+            val mDialogView = LayoutInflater.from(this).inflate(R.layout.custome_dialog, null)
+            val mBuilder = AlertDialog.Builder(this)
+                .setView(mDialogView)
+                .setTitle("운동 메모 다이얼로그")
+
+            val mAlertDialog = mBuilder.show()
+
+            val dateSelectBtn = mAlertDialog.findViewById<Button>(R.id.dateSelectBtn)
+
+            // mAlertDialog : 다이어로그
+            dateSelectBtn?.setOnClickListener {
+
+                val today = GregorianCalendar()
+                val year : Int = today.get(Calendar.YEAR)
+                val month : Int = today.get(Calendar.MONTH)
+                val date : Int = today.get(Calendar.DATE)
+
+                val dlg = DatePickerDialog(this, object : DatePickerDialog.OnDateSetListener{
+                    override fun onDateSet(p0: DatePicker?, year: Int, month: Int, day: Int) {
+                        Log.d("MAIN", "${year}, ${month+1}, ${day}")
+                        dateSelectBtn.setText("${year}, ${month+1}, ${day}")
+                    }
+                }, year, month, date)
+
+                dlg.show()
+            }
+
+        }
 
     }
 }
